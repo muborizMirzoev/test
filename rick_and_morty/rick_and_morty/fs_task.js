d7ebe3c19dd213f0360c3ccb9dd2d5fa67019fbf
muborizMirzoev <muboriz10@gmail.com>
2020-10-21 15:05:25 +0700

    test2

---
+++ rick_and_morty/rick_and_morty/fs_task.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function(){
+    console.clear()
+
+    function find(items, text) {
+        text = text.split(' ');
+        return items.filter(function(item) {
+            return text.every(function(el) {
+                return item.indexOf(el) > -1
+            })
+        })
+    }
+
+    const input = document.querySelector('.fs-task-input')
+    const tags = document.querySelectorAll('a')
+    const textContainer = []
+    const quantity = document.getElementById('lenthA');
+    const ul = document.querySelector('.fs-task__list');
+    
+    
+    tags.forEach(tag => tag.innerText && textContainer.push(tag.innerText) )
+    // console.log(textContainer);
+
+    input.addEventListener('input', updateValue);
+
+    function updateValue(e) { // Search something
+        const value = e.target.value;
+
+            let arrayA = (find(textContainer, value))
+
+            for(let item of arrayA) {
+                const p = document.createElement('p');
+                p.textContent = item;
+                ul.append(p);
+            }
+            
+    
+            let lengthA = (find(textContainer, value).length);
+            quantity.textContent = lengthA;
+    }
+    
+});
